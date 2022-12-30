894edc1c9a57fa27bb29801ffdacc969730200c1
aman774554 <aman774554@gmail.com>
2022-12-30 16:48:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react'
 function App() {
+    const [col, setCol] = useState('white');
+setTimeout(function(){
+    document.getElementById("one").style.background = col;
+  }, 2000);
+setTimeout(function(){
+  document.getElementById("two").style.background = col;
+}, 3000);
+setTimeout(function(){
+  document.getElementById("three").style.background = col;
+}, 4000);
+setTimeout(function(){
+  document.getElementById("four").style.background = col;
+}, 5000);
+setTimeout(function(){
+  document.getElementById("five").style.background = col;
+}, 6000);
+setTimeout(function(){
+setCol('black');
+}, 7000);
+
+  const rectangle = {
+  display: 'inline-block',
+  width: '50px',
+  height: '50px',
+  background: 'black',
+  marginRight: '10px',
+
+
+};
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  <div>
+  <table>
+  <tbody >
+  <tr>
+  <th id="one" style={rectangle}></th>
+  <th id="two" style={rectangle}></th>
+  <th id="three" style={rectangle}></th>
+  <th id="four" style={rectangle}></th>
+  <th id="five" style={rectangle}></th>
+  </tr>
+
+  <tr>
+    <th style={rectangle}></th>
+    <th style={rectangle}></th>
+    <th style={rectangle}></th>
+    <th style={rectangle}></th>
+    <th style={rectangle}></th>
+
+  </tr>
+  <tr>
+  <th style={rectangle}></th>
+  <th style={rectangle}></th>
+  <th style={rectangle}></th>
+  <th style={rectangle}></th>
+  <th style={rectangle}></th>
+  </tr>
+
+  </tbody>
+</table>
+</div>
   );
 }
 
